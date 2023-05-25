a22e566318ed179c28dbbdddf7eba5fce75cc2e8
Anna-Fong <78882159+Anna-Fong@users.noreply.github.com>
2023-05-25 15:51:29 -0700

    Add files via upload

--- GP1_final.py
+++ GP1_final.py
@@ -24,17 +24,19 @@ userNights = int(input("\nFor how many nights do you want to stay? --> "))
 userCulture = input("Do you like culture and classical music?\nPlease answer y/n. --> ")
 userActivity = input("Do you like beaches and surfing?\nPlease answer y/n. --> ")
 
-# Function to calculate total Bali cost
+# Variables to store cost information
 flightVienna = 997.93
 hotelVienna = 143.84
+flightBali = 849.93
+hotelBali = 235.35
 
+# Function to calculate total Vienna cost
 def viennaTotal():
     totalVienna = (flightVienna + hotelVienna * userNights) * (1-seniorDiscount)
-    print(totalVienna)
+    print(str(totalVienna)+ "$")
 
+# Function to calculate total Bali cost
 def baliTotal():
-    flightBali = 849.93
-    hotelBali = 235.35
     totalBali = (flightBali + hotelBali * userNights) * (1-seniorDiscount)
     print(totalBali)
 
@@ -46,11 +48,31 @@ def viennaInfo():
     print("Discount: " + str(int(seniorDiscount)) + "%")
     print("Total for", str(userNights) , " nights (incl. discounts): ", end="")
 
+# Function to display standard Bali costs
+def baliInfo():
+    print("\nHow about a trip to Bali?")
+    print("Flight: " + str(flightBali) + "$")
+    print("Hotel: " + str(hotelBali) + "$/night")
+    print("Discount: " + str(int(seniorDiscount)) + "%")
+    print("Total for", str(userNights) , " nights (incl. discounts): ", end="")
+
 # Make recommendations based on user's responses
-# If user prefers culture over beach -> Vienna
+# If user prefers culture over beach, recommend Vienna
 if userCulture == 'y' and userActivity == 'n':
     viennaInfo()
-    viennaTotal() #we need dollar sign here
-    
+    viennaTotal()
 
-  
+# If user prefers beach over culture, recommend Bali
+if userCulture == 'n' and userActivity == 'y':
+    baliInfo()
+    baliTotal()
+
+# If user prefers neither culture or beach, recommend none
+if userCulture == 'n' and userActivity == 'n':
+    print("\nI am sorry, we don't have any trips to offer at this point.")
+
+
+# If user prefers both culture and beaches, calculate more expensive trip and offer that to the user
+# else:
+    # should we do the calculation again in here? im not sure how to compare the costs in here
+
